738fa95fd4fe9ea6baf8f898feba92dde1cbe77e
Suzehva <suzeva@stanford.edu>
2024-08-12 22:25:09 -0700

    added new strategy for spotify API search

--- playlify/spot_search.py
+++ playlify/spot_search.py
@@ -1,6 +1,11 @@
+"""
+spot_search uses the Spotify API to turn a user's context into 
+songs to be used to form the user's sentence
+"""
+
 from typing import List
 import requests
-from constants import API_BASE_URL, NUM_SONGS
+from constants import API_BASE_URL
 
 def spot_search(session, context: str) -> List:
     """
@@ -10,25 +15,38 @@ def spot_search(session, context: str) -> List:
         context: the user's context which will be used to find songs
     
     Returns: a list of songs found including their URI's
-    """
-    results = []
 
-    context = context.split()
-    for word in context:
+    Ideas for improvement: 
+        - use wildcard % character
+        - play around with limit/offset more
+        - TODO: add genre filter to only find songs from user's genre
+    """
+    def call_spot(query: str, offset: int, limit: int):
         headers = {
             'Authorization': f"Bearer {session['access_token']}"
         }
         params = {
-            "q": word, # TODO (suze) at genre filter to only find songs from user's genre
+            "q": query,
+            "offset": offset,
             "type": "track",
-            "limit": NUM_SONGS
+            "limit": limit
         }
-        # TODO: (suze) make it so that spotify only finds titles as close to the input word as possible
-        
         responses = requests.get(API_BASE_URL + 'search', headers=headers, params=params)
         responses_json = responses.json()
         tracks = responses_json['tracks']['items']
         for track in tracks:
             results.append((track['name'], track['uri']))
-    return results
 
+    results = []
+    context = context.split()
+    context = list(set(context)) # remove duplicates
+
+    # call API with individual words
+    for word in context:
+        call_spot(query=word, offset=5, limit=5)
+
+    # try coupling up words
+    for i in range(len(context) - 1):
+        call_spot(query=context[i] + " " + context[i + 1], offset=0, limit=2)
+
+    return results
